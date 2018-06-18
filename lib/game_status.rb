98cff147226220de9ad463b698e5862d4b6e9c60
Margaret Dimond <margaretdimond@gmail.com>
2018-06-18 20:12:53 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -30,17 +30,17 @@ WIN_COMBINATIONS = [
 def won?(board)
     WIN_COMBINATIONS.each do |win_combination|
       win_index_1 = win_combination[0]
-      # win_index_2 = win_combination[1]
-      # win_index_3 = win_combination[2]
+      win_index_2 = win_combination[1]
+      win_index_3 = win_combination[2]
 
       position_1 = board[win_index_1] # load the value of the board at win_index_1
-      # position_2 = board[win_index_2] # load the value of the board at win_index_2
-      # position_3 = board[win_index_3] # load the value of the board at win_index_3
-      if position_1 == "X" 
-          return win_combination # return the win_combination indexes that won.
-        else
-          false
-        end
+      position_2 = board[win_index_2] # load the value of the board at win_index_2
+      position_3 = board[win_index_3] # load the value of the board at win_index_3
+      if position_1 == "X" && position_2 == "X" && position_3 == "X"
+        return win_combination # return the win_combination indexes that won.
+      else
+        false
+      end
     #  position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1) #detect returns first element (position_1) & make sure position is taken (that it's either an X or O).
    end
 end
