8042031f7269852c16a9a72cb830393e27a4ecd8
Margaret Dimond <margaretdimond@gmail.com>
2018-06-18 21:13:08 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -37,13 +37,12 @@ def won?(board)
       position_2 = board[win_index_2] # load the value of the board at win_index_2
       position_3 = board[win_index_3] # load the value of the board at win_index_3
 
-
       if position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1)
         return win_combination # return the win_combination indexes that won.
       else
         false
       end
-
+    end
 end
 
   # turn_count(board) > 1
