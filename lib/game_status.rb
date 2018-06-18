987e82e28ed7ba8129a4258d184dbc3fa8efa59f
Margaret Dimond <margaretdimond@gmail.com>
2018-06-18 20:55:24 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -40,7 +40,7 @@ def won?(board)
 
       if position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1)
          win_combination # return the win_combination indexes that won.
-      elsif !position_taken? (board,index)
+      elsif !position_taken?(board, index)
       end
 
 end
